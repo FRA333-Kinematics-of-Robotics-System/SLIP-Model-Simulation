ea3e0716c905c2eb955cbe59ae92626a3c1f7f17
TeeTy <vasayos.tsr@gmail.com>
2024-12-04 23:51:11 +0700

    update Pybullet_Simulation.py and SLIP_Model.urdf

--- SLIP/Pybullet_Simulation.py
+++ SLIP/Pybullet_Simulation.py
@@ -57,75 +57,87 @@ l_rest = r0
 k_spring = p.readUserDebugParameter(K_spring_id)
 
 x_i, y_i, z_i = mass_x, mass_y, mass_z
-time_step = 1 / 240.0
+time_step = 1 / 100.0
 dx, dy, dz = 1, 0, 0
 simulation_time = 0
 state = 1
 previous_r = r
 T_fight = 2 * dz / -g
 T_stance = 2*np.pi* np.sqrt(m_mass_id/k_spring)
-    
+
+cameraDistance = 6
+cameraYaw = 45
+cameraPitch = -30
+cameraTargetPosition = [0, 0, 0]
+
 print("System initialized. Waiting for release...")
-input("Press Enter to release the system.")
+
 t_sim = 0
+run = False
 
 while True:
-
-    if state == 1:
-        phase = "stance"
-        r, r_dot, theta, theta_dot, phi, phi_dot, mass_x, mass_y, mass_z = StancePhase(
-            m_mass_id, k_spring, r0, g, time_step, r, r_dot, theta, theta_dot, phi, phi_dot ,spring_x, spring_y, spring_z
-        )
-
-        if r >= r0 :
-            phase = "take-off"
-
-            x_i, y_i, z_i, dx, dy, dz, spring_x, spring_y, spring_z, theta_i, T_fight, t_sim, state = StanceToFlight(
-                mass_x, mass_y, mass_z, theta, phi, r_dot, r, theta_dot, phi_dot)
-            
-            phase = "fight"
-
-    elif (state == 2) & (phase == "fight"):
-        mass_x, mass_y, mass_z, spring_x, spring_y, spring_z, theta = FightPhase(
-            x_i, y_i, z_i, r, theta, phi, theta_i, t_sim, g, dx, dy, dz, T_fight)
-        
-        t_sim += time_step
-
-        if (spring_z <= 0) & (t_sim >= T_fight/2):
-            phase = "touchdown"
-
-            spring_z, x_i, y_i, z_i, dx, dy, dz, r_dot, phi_dot, theta_dot, T_stance, t_sim, state = FightToStance(
-                mass_x, mass_y, mass_z, r_dot, phi_dot, theta_dot, m_mass_id ,k_spring
+    keys = p.getKeyboardEvents()
+    if p.B3G_RETURN in keys and keys[p.B3G_RETURN] & p.KEY_WAS_TRIGGERED:
+        run = True
+    
+    if run:
+        p.resetDebugVisualizerCamera(cameraDistance, cameraYaw, cameraPitch, cameraTargetPosition)
+        if state == 1:
+            phase = "stance"
+            r, r_dot, theta, theta_dot, phi, phi_dot, mass_x, mass_y, mass_z = StancePhase(
+                m_mass_id, k_spring, r0, g, time_step, r, r_dot, theta, theta_dot, phi, phi_dot ,spring_x, spring_y, spring_z
             )
 
-            phase = "stance"
+            if r >= r0 :
+                phase = "take-off"
 
-    elif (state == 3) & (phase == "stance"):
-        r, r_dot, theta, theta_dot, phi, phi_dot, mass_x, mass_y, mass_z = StancePhase(
-            m_mass_id, k_spring, r0, g, time_step, r, r_dot, theta, theta_dot, phi, phi_dot, spring_x, spring_y, spring_z
-        )
-        t_sim += time_step
-        if (r >= r0) & (t_sim >= T_stance/2):
-            phase = "take-off"
+                x_i, y_i, z_i, dx, dy, dz, spring_x, spring_y, spring_z, theta_i, T_fight, t_sim, state = StanceToFlight(
+                    mass_x, mass_y, mass_z, theta, phi, r_dot, r, theta_dot, phi_dot)
+                
+                phase = "fight"
 
-            x_i, y_i, z_i, dx, dy, dz, spring_x, spring_y, spring_z, theta_i, T_fight, t_sim, state = StanceToFlight(
-                mass_x, mass_y, mass_z, theta, phi, r_dot, r, theta_dot, phi_dot)
+        elif (state == 2) & (phase == "fight"):
+            mass_x, mass_y, mass_z, spring_x, spring_y, spring_z, theta = FightPhase(
+                x_i, y_i, z_i, r, theta, phi, theta_i, t_sim, g, dx, dy, dz, T_fight)
             
-            phase = "fight"
+            t_sim += time_step
 
-    UpdateSimulation(spring_id, r, spring_x, spring_y, spring_z, theta, phi)
+            if (spring_z <= 0) & (t_sim >= T_fight/2):
+                phase = "touchdown"
 
-    print(f"Phase: {phase}")
-    print(f"Mass Position: x={mass_x}, y={mass_y}, z={mass_z}", f"Spring Base: x={spring_x}, y={spring_y}, z={spring_z}")
-    print(f"theta: {theta}, phi: {phi}")
-    print(f"length spring: r= {r}")
-    #print(f"time_phase: {t_sim}", f"T_fight: {T_fight}", f"T_stance: {T_stance}")
+                spring_z, x_i, y_i, z_i, dx, dy, dz, r_dot, phi_dot, theta_dot, T_stance, t_sim, state = FightToStance(
+                    mass_x, mass_y, mass_z, r_dot, phi_dot, theta_dot, m_mass_id ,k_spring
+                )
 
-    print(f"Angulavelocity: r_dot={r_dot}, theta_dot={theta_dot}, phi_dot={phi_dot}")
-    print("\n")
-    # print(f"velocity: x_dot={dx}, y_dot={dy}, z_dot={dz}")
-    
-    simulation_time += time_step
+                phase = "stance"
 
-    p.stepSimulation()
-    time.sleep(time_step)
+        elif (state == 3) & (phase == "stance"):
+            r, r_dot, theta, theta_dot, phi, phi_dot, mass_x, mass_y, mass_z = StancePhase(
+                m_mass_id, k_spring, r0, g, time_step, r, r_dot, theta, theta_dot, phi, phi_dot, spring_x, spring_y, spring_z
+            )
+            t_sim += time_step
+            if (r >= r0) & (t_sim >= T_stance/2):
+                phase = "take-off"
+
+                x_i, y_i, z_i, dx, dy, dz, spring_x, spring_y, spring_z, theta_i, T_fight, t_sim, state = StanceToFlight(
+                    mass_x, mass_y, mass_z, theta, phi, r_dot, r, theta_dot, phi_dot)
+                
+                phase = "fight"
+
+        UpdateSimulation(spring_id, r, spring_x, spring_y, spring_z, theta, phi)
+        cameraTargetPosition = [spring_x, spring_y, 0]
+        
+        print(f"Phase: {phase}")
+        print(f"Mass Position: x={mass_x}, y={mass_y}, z={mass_z}", f"Spring Base: x={spring_x}, y={spring_y}, z={spring_z}")
+        print(f"theta: {theta}, phi: {phi}")
+        print(f"length spring: r= {r}")
+        #print(f"time_phase: {t_sim}", f"T_fight: {T_fight}", f"T_stance: {T_stance}")
+
+        print(f"Angulavelocity: r_dot={r_dot}, theta_dot={theta_dot}, phi_dot={phi_dot}")
+        print("\n")
+        # print(f"velocity: x_dot={dx}, y_dot={dy}, z_dot={dz}")
+        
+        simulation_time += time_step
+
+        p.stepSimulation()
+        time.sleep(time_step)
